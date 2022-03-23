0e6008afdc2200332ba16fbb89ed0647fe66bb15
Kris <kris.kodehode@gmail.com>
2022-03-23 13:26:34 +0100

    added proper placeholder image

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import "./home.css"
-import bulki from "../images/bulki.png"
+import placeholderProfile from "../images/placeholderProfile.png"
 import HTML5 from "../images/logos.png"
 
 export default function Home() {
     return (
         <div className="home">
-            <img className="profile-img" src={bulki} alt="profile" />
+            <img className="profile-img" src={placeholderProfile} alt="profile" />
             <div className="text-container">
                 <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolor pariatur, distinctio commodi ut itaque at suscipit voluptatum provident. Tempora libero mollitia nam porro magni.</h3>
                 <img className="logo" src={HTML5} alt="HTML5 Logo" />
